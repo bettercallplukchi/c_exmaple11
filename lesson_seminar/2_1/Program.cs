fd826374771d1ca13a7b8934ee6f873985ee81fe
Sasha Plukchi <loxpojizniia@gmail.com>
2023-12-21 20:25:33 +0200

    Lesson 2 task 1

--- lesson_seminar/2_1/Program.cs
+++ lesson_seminar/2_1/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System.Runtime.Serialization;
+
+string a1 = Console.ReadLine()!;
+string a2 = Console.ReadLine()!;
+
+int b1 = Convert.ToInt32(a1);
+int b2 = Convert.ToInt32(a2);
+
+if (b1 * b1 == b2)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
